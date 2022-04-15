b2a7dcab95302faec980489fa2707080d46eeb5c
Brent Reeves <brent@twinbear.com>
2022-04-15 17:37:25 -0500

    /book/Oba/study/1

--- app/db/book.js
+++ app/db/book.js
@@ -12,6 +12,15 @@ const u = require("../util/utils");
  RowCtor: null,
  rowsAsArray: false
 */
+
+exports.findDistinct = async () => {
+    u.log(3,"/db/book.js findAll...");
+    let sql = 'select distinct ot_book from ot_book_study order by ot_book'
+    var rs = await db.query( sql, null )
+    u.log(4,`  db/book findDistinct  data: ${JSON.stringify(rs)}`);
+    return {"msg": "", "rows": rs.rows};
+}
+
 exports.findAll = async () => {
     u.log(3,"/db/book.js findAll...");
     let sql = 'select ot_book, study_no, description, date_created, mss_used from ot_book_study order by ot_book, study_no'
@@ -26,5 +35,14 @@ exports.findOne = async (book) => {
     var rs = await db.query(sql, [book])
     u.log(4,`  book findOne  sql: ${sql}`);
     u.log(4,`  book findOne  data: ${JSON.stringify(rs)}`);
+    return {"msg": "", "rows": rs.rows};
+}
+
+exports.findStudy = async (book, study) => {
+    u.log(3, `book.js findStudy ... book: ${book} study: ${study}`);
+    let sql = 'select ot_book, study_no, description, date_created, mss_used from ot_book_study where ot_book = $1 and study_no = $2 order by ot_book, study_no';
+    var rs = await db.query(sql, [book, study])
+    u.log(4,`  book findStudy  sql: ${sql}`);
+    u.log(4,`  book findStudy  data: ${JSON.stringify(rs)}`);
     return {"msg": "", "rows": rs.rows};
 }
--- app/routes/book.routes.js
+++ app/routes/book.routes.js
@@ -1,7 +1,7 @@
 const u = require("../util/utils")
 
 module.exports = function(app) {
-    u.log(1,'book.routes.js !! ...');
+    u.log(1,'book.routes.js ...');
     
     const book = require("../db/book.js");
     var router = require("express").Router();
@@ -17,9 +17,24 @@ module.exports = function(app) {
     //   next();
     // });
 
+    router.get("/:book/study/:study", async (req,res) => {
+	let abook = req.params.book
+	let astudy = req.params.study
+	u.log(3,`book.routes  /:book ${abook} /:study ${astudy}`)
+	let rs = await book.findStudy(abook, astudy)
+	u.log(3,`book.routes /:book/study:study rs: ${JSON.stringify(rs)}`)
+	u.log(3,`book.routes /:book/study:study rows: ${JSON.stringify(rs.rows)}`)
+	u.log(3,`book.routes /:book/study:study rows[0]: ${JSON.stringify(rs.rows[0])}`)
+
+	// let headers = []
+	// if (rs.rows.length > 0)
+	//     headers = rs.rows[0].verses
+	res.render('pages/study.ejs', {"study" : rs.rows[0], "book": abook, "studyno": astudy});
+    });
+
     router.get("/:book", async (req,res) => {
 	let abook = req.params.book
-	u.log(3,`book.routes ! /:book ${abook}`)
+	u.log(3,`book.routes /:book ${abook}`)
 	let rs = await book.findOne(abook)
 
 	u.log(3,`book.routes /:book rs: ${JSON.stringify(rs)}`)
@@ -27,17 +42,17 @@ module.exports = function(app) {
 	let headers = []
 	if (rs.rows.length > 0)
 	    headers = rs.rows[0].verses
-	res.render('pages/book.ejs', {"rows" : rs.rows, "cols" : headers, "book": abook});
+	res.render('pages/book.ejs', {"rows" : rs.rows, "book": abook});
     });
 
     router.get("/", async (req,res) => {
-	var rs = await book.findAll()
+	var rs = await book.findDistinct()
 	u.log(3,`book.routes ! / rs: ${JSON.stringify(rs)}`)
 	u.log(3,`book.routes ! / rows: ${JSON.stringify(rs.rows)}`)
 	let headers = []
 	if (rs.rows.length > 0)
 	    headers = rs.rows[0].verses
-	res.render('pages/books.ejs', {"rows" : rs.rows, "cols" : headers});
+	res.render('pages/books.ejs', {"rows" : rs.rows});
     })
 
     app.use("/book", router);
